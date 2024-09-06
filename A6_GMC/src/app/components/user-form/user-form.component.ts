74dbb0456dae73a9b935d9d9edad532a4cce2402
Gabriel Martí Capell <electro.gnu@gmail.com>
2024-09-06 21:40:23 +0100

    Set validators in forms

--- A6_GMC/src/app/components/user-form/user-form.component.ts
+++ A6_GMC/src/app/components/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsersService } from '../../services/users.service';
 import { Iuser } from '../../interfaces/iuser.interface';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -13,43 +13,55 @@ import Swal from 'sweetalert2';
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
-  // dependency injection
+
   userService = inject(UsersService);
   router = inject(Router);
   activatedRoute = inject(ActivatedRoute);
-
-  tipoFormulario: string = 'Insertar';
   userForm: FormGroup;
+  formType: string = 'Insertar';
 
-
-  /// falta los validadores
+  /**
+   * Constructor. Creates a new FormGroup with the following fields:
+   * first_name, last_name, email and image.
+   * No validators used here, rewrited in ngOnInit funtion
+   */
   constructor() {
     this.userForm = new FormGroup({
       first_name: new FormControl(null, []),
       last_name: new FormControl(null, []),
       email: new FormControl(null, []),
       image: new FormControl(null, [])
-
-    })
+    },
+      [])
   }
 
+  /**
+   * Lifecycle hook that is called when the component is initialized.
+   * Checks if the URL contains an id parameter. If so, it sets the formType to 'Actualizar'
+   * and using the UserService.getUserById() initializes the form  with the loaded user's data. 
+   * If not, it sets the formType to 'Insertar' and the form is set empty.
+   */
   ngOnInit() {
-    // Preguntar por parametros de ruta.
-    // si no tengo parametros
     this.activatedRoute.params.subscribe(async (params: any) => {
       if (params.id) {
-        this.tipoFormulario = 'Actualizar';
+        this.formType = 'Actualizar';
         ///llamar acargar usuario
         const user: Iuser = await this.userService.getUserById(params.id);
         this.userForm = new FormGroup({
           _id: new FormControl(user._id, []),
-          first_name: new FormControl(user.first_name, []),
-          last_name: new FormControl(user.last_name, []),
-          email: new FormControl(user.email, []),
-          image: new FormControl(user.image, [])
-        })
+          first_name: new FormControl(user.first_name, [Validators.required, Validators.minLength(3)]),
+          last_name: new FormControl(user.last_name, [Validators.required, Validators.minLength(3)]),
+          email: new FormControl(user.email, [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]),
+          image: new FormControl(user.image, [Validators.required, Validators.pattern(/^https:\/\/i\.pravatar\.cc\/\d+\?u=[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)])
+        }, [])
       } else {
-        this.tipoFormulario = 'Insertar';
+        this.formType = 'Insertar';
+        this.userForm = new FormGroup({
+          first_name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+          last_name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+          email: new FormControl(null, [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]),
+          image: new FormControl(null, [Validators.required, Validators.pattern(/^https:\/\/i\.pravatar\.cc\/\d+\?u=[a-zA-Z0-9._%+-]/)])
+        })
       }
     })
 
@@ -57,12 +69,12 @@ export class UserFormComponent {
 
   async getDataForm() {
 
-    if (this.userForm.value._id) {  // actualizando usuario
-      console.log('Actualizando usuario')
+    if (this.userForm.value._id) {
+      // Actualize user
       try {
         const resp: Iuser = await this.userService.update(this.userForm.value);
         if (resp._id) {
-          let sar = await Swal.fire({
+          const sar = await Swal.fire({
             title: 'Usuario actualizado con exito',
             text: 'Usuario:' + resp.first_name + ' ' + resp.last_name,
             icon: 'success',
@@ -71,22 +83,19 @@ export class UserFormComponent {
           this.router.navigate(['/home']);
         }
       } catch (error) {
-        let sar = await Swal.fire({
+        const sar = await Swal.fire({
           title: 'Ups! Algo no fue bien, contacta el administrador!',
           text: 'error:' + error,
           icon: 'error',
         })
-        console.log(error);
+
       }
-      console.log(this.userForm.value._id)
     } else {
-      console.log('--> Insertando usuario')
+      // Insert user
       try {
         const resp: Iuser = await this.userService.insert(this.userForm.value);
-        console.log(resp);
         if (resp.id) {
-          // recargar la lista de usuarios si nos vamos a la pagina actual
-          let sar = await Swal.fire({
+          const sar = await Swal.fire({
             title: 'Usuario insertado con exito',
             text: 'Usuario:' + resp.first_name + ' ' + resp.last_name,
             icon: 'success',
@@ -95,12 +104,11 @@ export class UserFormComponent {
           this.router.navigate(['/home']);
         }
       } catch (error) {
-        let sar = await Swal.fire({
+        const sar = await Swal.fire({
           title: 'Ups! Algo no fue bien, contacta el administrador!',
           text: 'error:' + error,
           icon: 'error',
         })
-        console.log(error);
       }
     }
   }
